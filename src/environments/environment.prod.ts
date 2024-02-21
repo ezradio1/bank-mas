3aa301561d64b794091419dca5b3d9bde45f2cc9
ezradio1 <ezraaudivano@gmail.com>
2024-02-21 10:21:24 +0700

    chore: add env variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseApi: 'https://bank-mas-dd6b6-default-rtdb.firebaseio.com/',
+  firebaseAuthUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:',
+  firebaseApiKey: 'AIzaSyD0mfqTMeijSsup4F-3Os1NgqDRZUyNCc4',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseApi: 'https://bank-mas-dd6b6-default-rtdb.firebaseio.com/',
+  firebaseAuthUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:',
+  firebaseApiKey: 'AIzaSyD0mfqTMeijSsup4F-3Os1NgqDRZUyNCc4',
 };
 
 /*
