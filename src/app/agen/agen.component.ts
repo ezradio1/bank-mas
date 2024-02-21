553238e552df56c4daf6d9c1c5721134105eb164
ezradio1 <ezraaudivano@gmail.com>
2024-02-21 10:29:43 +0700

    feat: create agen menu

---
+++ src/app/agen/agen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agen',
+  templateUrl: './agen.component.html',
+  styleUrls: ['./agen.component.scss']
+})
+export class AgenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
